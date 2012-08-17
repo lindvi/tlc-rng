813c09512b9d1747830ce9c312b338162e07f592
Charlie Lindviken <lindvi@130-229-38-156-dhcp.kfa.ki.se>
2012-08-17 10:02:51 +0200

    new rand

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Randomization::Application.config.secret_token = 'c0023425b91feb0e25f02b6c5ecbeab6a65f12fc7910ac02ac7879db5b0bca7019ca5c6c6a9b856287ad849fe2c909141e1fffc34e9e61e5520411f80c7eed2e'
